d739cddf662e029f7693801a9b574d55947ccecf
Brian G <brian.gregorius@gmail.com>
2018-12-06 12:57:46 -0500

    setup of server and first tests

---
+++ index.spec.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+
+const server = require('./api/server.js');
+
+describe('server', () => {
+
+    describe('GET /', () => {
+        it('should return status code 200(ok)', async () => {
+            const response = await request(server).get('/');
+            expect(response.status).toBe(200);
+        });
+    });
+
+    it('can run tests', () => {
+        expect(true).toBeTruthy();
+    });
+
+    it('can run more tests', () => {
+        expect(false).toBeFalsy();
+    });
+});
+
